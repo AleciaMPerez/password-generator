92fbe88a1c54b3579f57f53438d40deae641dee0
AleciaMPerez <alecia.perez@hotmail.com>
2023-04-29 13:23:30 -0400

    Created functions for random characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,32 @@
-// Assignment code here
+// Random object
+
+var randomFunction= {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  symbol: randomSymbol
+}
+
+
+// Functions to return random outputs.
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) +97)
+}
+
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) +65)
+}
+
+function randomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() *10) +48)
+}
+
+function randomSymbol(){
+  return String.fromCharCode(Math.floor(Math.random() *15) +33)
+}
+
+
+
 
 
 // Get references to the #generate element
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Password Generator Starter Code
+Character number value chart - https://www.w3schools.com/html/html_charset.asp
+
+Youtube - https://www.google.com/search?q=password+generator+javascript&rlz=1C1RXMK_enUS1052US1052&oq=password+generator+javascript&aqs=chrome..69i57j0i512j0i20i263i512j0i512l3j69i60l2.8161j1j7&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:4ccfc666,vid:duNmhKgtcsI
+
